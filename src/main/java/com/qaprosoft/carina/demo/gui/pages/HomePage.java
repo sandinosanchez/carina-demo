6dbfe15cad7df551f61c2d93153477a33b982acc
Sandino Sanchez <43247306+sandinosanchez@users.noreply.github.com>
2020-07-06 10:27:33 -0300

    Update HomePage.java

--- src/main/java/com/qaprosoft/carina/demo/gui/pages/HomePage.java
+++ src/main/java/com/qaprosoft/carina/demo/gui/pages/HomePage.java
@@ -36,7 +36,7 @@ public class HomePage extends AbstractPage {
     @FindBy(xpath = "//div[contains(@class, 'brandmenu-v2')]//a")
     private List<ExtendedWebElement> brandLinks;
     
-    private String st;
+    private String st = null;
 
     public HomePage(WebDriver driver) {
         super(driver);
